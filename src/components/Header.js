ae238f906e34948feb86556e90e40e0862a04543
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-14 11:35:52 -0700

    styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+const HelpQueueHeader = styled.h1`
+  font-size: 24px;
+  text-align: center;
+  color: black;
+`;
+
+const StyledWrapper = styled.section`
+  background-color: orange;
+`;
 
 function Header(){
   return (
-    <React.Fragment>
-      <h1> Help Queue</h1>
+    <StyledWrapper>
+    <>
+      <HelpQueueHeader>
+        Help Queue
+      </HelpQueueHeader>
       <ul>
         <li>
           <Link to="/">Home</Link>
@@ -13,7 +27,8 @@ function Header(){
           <Link to="/signin">Sign In</Link>
         </li>
       </ul>
-    </React.Fragment>
+    </>
+    </StyledWrapper>
   );
 }
 
